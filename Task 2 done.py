32824d70e2930d276637f2c3d3bed82ecddf55cc
Simar bhatia <simar74bhatia@gmail.com>
2025-03-31 16:00:48 +0530

    Add files via upload

---
+++ Task 2 done.py
@@ -0,0 +1,10 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Task 2
+>>> a=input('enter the first name ')
+enter the first name simar
+>>> b=input('enter the second name ')
+enter the second name Kaur
+>>> 
+>>> print("Hello, Simar Kaur! Welcome to the First programe.")
+Hello, Simar Kaur! Welcome to the First programe.
